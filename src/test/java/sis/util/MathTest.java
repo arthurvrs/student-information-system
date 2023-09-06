d7a46c9ee110eb3e3df6c014724dee492a72c0b1
Arthur Santos <avrs@ic.ufal.br>
2023-09-06 11:27:12 -0300

    Capítulo 10

--- src/test/java/sis/util/MathTest.java
+++ src/test/java/sis/util/MathTest.java
@@ -2,6 +2,8 @@ package sis.util;
 
 import junit.framework.TestCase;
 
+import java.util.*;
+
 public class MathTest extends TestCase {
 
     static final double TOLERANCE = 0.05;
@@ -9,5 +11,22 @@ public class MathTest extends TestCase {
     public void testHypotenuse() {
         assertEquals(5.0, Math.hypotenuse(3.0, 4.0), TOLERANCE);
     }
+
+    public void testCoinFlips() {
+        final long seed = 100L;
+        final int total = 10;
+        Random random1 = new Random(seed);
+        List<Boolean> flips1 = new ArrayList<>();
+        for(int i = 0; i < total; i++)
+            flips1.add(random1.nextBoolean());
+
+        Random random2 = new Random(seed);
+        List<Boolean> flips2 = new ArrayList<>();
+        for(int i = 0; i < total; i++)
+            flips2.add(random2.nextBoolean());
+
+        assertEquals(flips1, flips2);
+    }
+
 
 }
